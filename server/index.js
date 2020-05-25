d0c12788ff3b96371dfd91602b450b7686afdb0d
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-25 13:06:27 +0530

    Moving schema logic to schema.js from server/index.js

--- server/index.js
+++ server/index.js
@@ -1,81 +1,19 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import * as graphql from 'graphql';
-import camelCase from 'lodash/camelCase';
-import pluralize from 'pluralize';
+import { GraphQLSchema, GraphQLObjectType } from 'graphql';
 import dotenv from 'dotenv';
-import joinMonster from 'join-monster';
-import { client, connect } from 'database';
-import { Item, ItemConnection } from 'models/items';
-import { PurchasedItem, PurchasedItemConnection } from 'models/purchasedItems';
-import { Address, AddressConnection } from 'models/addresses';
-import { StoreItem, StoreItemConnection } from 'models/storeItems';
-import { Store, StoreConnection } from 'models/stores';
-import { Supplier, SupplierConnection } from 'models/suppliers';
-import { SupplierItem, SupplierItemConnection } from 'models/supplierItems';
+
+import { connect } from 'database';
 import { nodeField } from './node';
+import { addQueries } from './schema';
 
-const DB_TABLES = {
-    Item,
-    PurchasedItem,
-    Address,
-    StoreItem,
-    Store,
-    Supplier,
-    SupplierItem
-};
+// configure environment variables
+dotenv.config({ path: `.env.${process.env.ENVIRONMENT}` });
 
-const CONNECTIONS = {
-    Item: ItemConnection,
-    PurchasedItem: PurchasedItemConnection,
-    Address: AddressConnection,
-    StoreItem: StoreItemConnection,
-    Store: StoreConnection,
-    Supplier: SupplierConnection,
-    SupplierItem: SupplierItemConnection
-};
-
-const dotEnvFile = `.env.${process.env.ENVIRONMENT}`;
-
-dotenv.config({ path: dotEnvFile });
-
+// connect to database
 connect();
-const options = { dialect: 'pg' };
-const addQueries = () => {
-    const query = {};
-    Object.keys(DB_TABLES).forEach(table => {
-        query[camelCase(table)] = {
-            type: DB_TABLES[table],
-            args: {
-                id: { type: graphql.GraphQLNonNull(graphql.GraphQLInt) }
-            },
-            where: (t, args, context) => `${t}.id = ${args.id}`,
-            resolve: (parent, args, context, resolveInfo) =>
-                joinMonster(resolveInfo, {}, sql => client.query(sql))
-        };
-        query[pluralize(camelCase(table))] = {
-            type: CONNECTIONS[table],
-            args: {
-                first: {
-                    type: graphql.GraphQLInt
-                },
-                after: {
-                    type: graphql.GraphQLString
-                },
-                before: {
-                    type: graphql.GraphQLString
-                }
-            },
-            sqlPaginate: true,
-            orderBy: 'id',
-            resolve: (parent, args, context, resolveInfo) =>
-                joinMonster(resolveInfo, {}, sql => client.query(sql), options)
-        };
-    });
-    return query;
-};
 
-const QueryRoot = new graphql.GraphQLObjectType({
+const QueryRoot = new GraphQLObjectType({
     name: 'Query',
     fields: () => ({
         node: nodeField,
@@ -83,7 +21,7 @@ const QueryRoot = new graphql.GraphQLObjectType({
     })
 });
 
-const schema = new graphql.GraphQLSchema({ query: QueryRoot });
+const schema = new GraphQLSchema({ query: QueryRoot });
 
 const app = express();
 app.use(
---
+++ server/schema.js
@@ -0,0 +1,132 @@
+import { GraphQLNonNull, GraphQLInt, GraphQLString } from 'graphql';
+import camelCase from 'lodash/camelCase';
+import pluralize from 'pluralize';
+import joinMonster from 'join-monster';
+import { client } from 'database';
+import { Item, ItemConnection } from 'models/items';
+import { PurchasedItem, PurchasedItemConnection } from 'models/purchasedItems';
+import { Address, AddressConnection } from 'models/addresses';
+import { StoreItem, StoreItemConnection } from 'models/storeItems';
+import { Store, StoreConnection } from 'models/stores';
+import { Supplier, SupplierConnection } from 'models/suppliers';
+import { SupplierItem, SupplierItemConnection } from 'models/supplierItems';
+
+const DB_TABLES = {
+    Item: {
+        table: Item
+    },
+    PurchasedItem: {
+        table: PurchasedItem
+    },
+    Address: {
+        table: Address
+    },
+    StoreItem: {
+        table: StoreItem
+    },
+    Store: {
+        table: Store
+    },
+    Supplier: {
+        table: Supplier,
+        where: (t, args, context) => {
+            let where = ``;
+            if (args.itemId) {
+                where += `and ${t}.item_id=${args.itemId}`;
+            }
+            return escape(`${t}.id=${args.id} ${where}`);
+        },
+        args: {
+            itemId: {
+                type: GraphQLString
+            }
+        }
+    },
+    SupplierItem: {
+        table: SupplierItem
+    }
+};
+
+const CONNECTIONS = {
+    Item: {
+        list: ItemConnection
+    },
+    PurchasedItem: {
+        list: PurchasedItemConnection
+    },
+    Address: {
+        list: AddressConnection
+    },
+    StoreItem: {
+        list: StoreItemConnection
+    },
+    Store: {
+        list: StoreConnection
+    },
+    Supplier: {
+        list: SupplierConnection,
+        where: (t, args, context, aliases) => {
+            const where = ``;
+            if (args.itemId) {
+                // where +=`${t}.item_id = ${args.itemId}`
+            }
+            return `${where}`;
+        },
+        args: {
+            itemId: {
+                type: GraphQLInt
+            }
+        }
+    },
+    SupplierItem: {
+        list: SupplierItemConnection
+    }
+};
+
+const options = { dialect: 'pg' };
+export const addQueries = () => {
+    const query = {};
+    Object.keys(DB_TABLES).forEach(table => {
+        query[camelCase(table)] = {
+            type: DB_TABLES[table].table,
+            args: {
+                ...DB_TABLES[table].args,
+                id: { type: GraphQLNonNull(GraphQLInt) }
+            },
+            where: (t, args, context, aliases) => {
+                if (DB_TABLES[table].where) {
+                    return DB_TABLES[table].where(t, args, context, aliases);
+                }
+                return `${t}.id = ${args.id}`;
+            },
+            resolve: (parent, args, context, resolveInfo) =>
+                joinMonster(resolveInfo, {}, sql => client.query(sql), options)
+        };
+        query[pluralize(camelCase(table))] = {
+            type: CONNECTIONS[table].list,
+            args: {
+                first: {
+                    type: GraphQLInt
+                },
+                after: {
+                    type: GraphQLString
+                },
+                before: {
+                    type: GraphQLString
+                },
+                ...CONNECTIONS[table].args
+            },
+            where: (t, args, context) => {
+                if (CONNECTIONS[table].where) {
+                    return CONNECTIONS[table].where(t, args, context);
+                }
+                return ``;
+            },
+            sqlPaginate: true,
+            orderBy: 'id',
+            resolve: (parent, args, context, resolveInfo) =>
+                joinMonster(resolveInfo, {}, sql => client.query(sql), options)
+        };
+    });
+    return query;
+};
