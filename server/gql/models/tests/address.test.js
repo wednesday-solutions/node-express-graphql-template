67da30f1d0140a096476466d894a1b13b5225a46
praveenchand-wed <praveen@wednesday.is>
2020-11-05 01:00:27 +0530

    add test for mutation

--- server/gql/models/tests/address.test.js
+++ server/gql/models/tests/address.test.js
@@ -29,20 +29,20 @@ const query = `
 `;
 describe('Address introspection tests', () => {
   it('should have the correct fields and types', async () => {
-    const result = await graphqlSync({ schema, source: query });
+    const result = await graphqlSync({ schema, query });
     const addressFieldTypes = get(result, 'data.__type.fields');
     const hasCorrectFieldTypes = expectSameTypeNameOrKind(addressFieldTypes, fields);
     expect(hasCorrectFieldTypes).toBeTruthy();
   });
   it('should have a supplier connection', async () => {
-    const result = await graphqlSync({ schema, source: query });
+    const result = await graphqlSync({ schema, query });
     const addressFieldTypes = get(result, 'data.__type.fields');
     const supplierField = addressFieldTypes.find(field => field.name === 'suppliers');
     expect(supplierField.type.kind).toBe('OBJECT');
     expect(supplierField.type.name).toBe('SupplierConnection');
   });
   it('should have a store connection', async () => {
-    const result = await graphqlSync({ schema, source: query });
+    const result = await graphqlSync({ schema, query });
     const addressFieldTypes = get(result, 'data.__type.fields');
     const storeField = addressFieldTypes.find(field => field.name === 'stores');
     expect(storeField.type.kind).toBe('OBJECT');
