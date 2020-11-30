0644d13e861023921e4587aab84251e5b40b6d84
Mac <mohammed.ali.chherawalla@gmail.com>
2020-11-30 10:44:48 +0530

    adding checks to make sure health check passes

--- server/gql/models/tests/addresses/mutation.test.js
+++ server/gql/models/tests/addresses/mutation.test.js
@@ -46,6 +46,26 @@ describe('Address graphQL-server-DB mutation tests', () => {
   }
 `;
 
+  const updateAddressMutation = `
+  mutation {
+    updateAddress (
+      id: ${addressesTable[0].id}
+      address1: "${addressesTable[0].address1}"
+      address2: "${addressesTable[0].address2}",
+      lat: ${addressesTable[0].lat}
+      long: ${addressesTable[0].long}
+    ) {
+      id
+    }
+  }
+`;
+
+  const deleteAddressMutation = `mutation DeleteAddress {
+    deleteAddress(id: ${addressesTable[0].id}) {
+      id
+    }
+    
+  }`;
   it('should have a mutation to create a new address', async () => {
     jest.spyOn(dbClient.models.addresses, 'create');
     const response = await getResponse(createAddressMut);
@@ -57,6 +77,42 @@ describe('Address graphQL-server-DB mutation tests', () => {
       ...address,
       lat: parseFloat(address.lat),
       long: parseFloat(address.long)
+    });
+  });
+
+  it('should have a mutation to delete an address', async () => {
+    jest.spyOn(dbClient.models.addresses, 'destroy');
+    const response = await getResponse(deleteAddressMutation);
+    const result = get(response, 'body.data.deleteAddress');
+    expect(result).toBeTruthy();
+    expect(dbClient.models.addresses.destroy.mock.calls.length).toBe(1);
+    expect(dbClient.models.addresses.destroy.mock.calls[0][0]).toEqual({
+      where: {
+        deletedAt: null,
+        id: parseInt(addressesTable[0].id)
+      }
+    });
+  });
+
+  it('should have a mutation to update a new address', async () => {
+    jest.spyOn(dbClient.models.addresses, 'update');
+    const response = await getResponse(updateAddressMutation);
+    const result = get(response, 'body.data.updateAddress');
+    expect(result).toBeTruthy();
+    expect(dbClient.models.addresses.update.mock.calls.length).toBe(1);
+    console.log(dbClient.models.addresses.update.mock.calls[0][0]);
+    expect(dbClient.models.addresses.update.mock.calls[0][0]).toEqual({
+      id: addressesTable[0].id.toString(),
+      address1: addressesTable[0].address1,
+      address2: addressesTable[0].address2,
+      lat: parseFloat(addressesTable[0].lat),
+      long: parseFloat(addressesTable[0].long)
+    });
+    expect(dbClient.models.addresses.update.mock.calls[0][1]).toEqual({
+      where: {
+        id: addressesTable[0].id.toString(),
+        deletedAt: null
+      }
     });
   });
 });
