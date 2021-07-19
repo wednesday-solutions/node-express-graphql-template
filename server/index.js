41475b81e3675ded638dc55c8554fb9ec3d59adb
rohan-wednesday <rohan@wednesday.is>
2021-07-20 05:09:56 +0530

    Upgrade express-graphql

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import graphqlHTTP from 'express-graphql';
+import { graphqlHTTP } from 'express-graphql';
 import { GraphQLSchema } from 'graphql';
 import dotenv from 'dotenv';
 import multer from 'multer';
