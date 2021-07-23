3ad94a37125f756b217c19a15b7c648077659c34
Chris <christin@wednesday.is>
2021-07-23 10:44:04 +0530

    test: getFileNames test: createModuleGetter
    Signed-off-by: Chris <christin@wednesday.is>

--- server/utils/tests/index.test.js
+++ server/utils/tests/index.test.js
@@ -1,4 +1,4 @@
-import { addWhereClause, isTestEnv, totalConnectionFields } from '@utils/index';
+import { addWhereClause, isTestEnv, totalConnectionFields, getFileNames, createModelGetter } from '@utils/index';
 
 describe('isTestEnv', () => {
   it("should return true if the ENVIRONMENT is 'test'", () => {
@@ -16,4 +16,63 @@ describe('addWhereClause', () => {
 
 describe('totalConnectionFields', () => {
   expect(totalConnectionFields.connectionFields.total.resolve({ fullCount: 10 })).toBe(10);
+});
+
+describe('getFileNames', () => {
+  it('should get file names of a directory', () => {
+    const fileNames = getFileNames('./server/database/models');
+    expect(fileNames[0]).toBe('addresses');
+  });
+
+  it('should ignore test folders', () => {
+    const fileNames = getFileNames('./server/utils');
+    const hasTest = fileNames.includes('tests');
+    expect(hasTest).toBeFalsy();
+  });
+
+  it('should ignore index files', () => {
+    const fileNames = getFileNames('./server/database/models');
+    const hasIndex = fileNames.includes('index');
+    expect(hasIndex).toBeFalsy();
+  });
+
+  it('should call readdirSync with the dir name', () => {
+    const readdirSyncSpy = jest.spyOn(require('fs'), 'readdirSync');
+    getFileNames('./server/database/models');
+    expect(readdirSyncSpy).toBeCalledWith('./server/database/models', { withFileTypes: true });
+  });
+});
+
+describe('createModelGetter', () => {
+  it('should throw an Error if the append parameter is invalid', () => {
+    expect(() => createModelGetter({ tables: {}, append: 'invalid' })).toThrow();
+  });
+
+  it('should not throw an Error if the append parameter is Queries / Mutations', () => {
+    expect(() => createModelGetter({ tables: {}, append: 'Queries' })).not.toThrow();
+    expect(() => createModelGetter({ tables: {}, append: 'Mutations' })).not.toThrow();
+  });
+
+  it('should throw an Error if the tables parameter is missing', () => {
+    expect(() => createModelGetter({ tabeles: {}, append: 'Queries' })).toThrow();
+  });
+
+  describe('should return a function', () => {
+    const tables = {};
+    const getter = createModelGetter({ tables, append: 'Queries' });
+    expect(typeof getter).toBe('function');
+    const model = {};
+    const modelQueries = jest.fn();
+    it('should convert the fileName param into camelCase and set the value returned by the getter in tables', () => {
+      model.notCamelQueries = modelQueries;
+      getter('not-camel', model);
+      expect(tables).toHaveProperty('notCamel', modelQueries);
+    });
+
+    it('should throw an Error if the getter is not available', () => {
+      delete model.notCamelQueries;
+      model.nottCamelQueries = modelQueries;
+      expect(() => getter('not-camel', model)).toThrow();
+    });
+  });
 });
