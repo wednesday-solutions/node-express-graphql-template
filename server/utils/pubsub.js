17f61655f1b08df7a2f3065943f81588a565e5fe
Anurag-Wednesday <anurag@wednesday.is>
2022-03-07 20:19:10 +0530

    added tests for pubsub

--- server/utils/pubsub.js
+++ server/utils/pubsub.js
@@ -1,7 +1,7 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import Redis from 'ioredis';
 
-const options = {
+export const options = {
   host: process.env.REDIS_DOMAIN,
   port: process.env.REDIS_PORT,
   connectTimeout: 10000,
--- server/utils/tests/pubsub.test.js
+++ server/utils/tests/pubsub.test.js
@@ -1,23 +1,30 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import Redis from 'ioredis';
-
+import { options } from '@utils/pubsub';
 describe('Pubsub tests', () => {
   let pubsub;
-  afterEach(() => pubsub.close());
+
+  afterAll(() => pubsub.close());
   it('should a pubsub stream ', () => {
-    const options = {
+    const mockOptions = {
       host: 'localhost',
       port: '6378',
       connectTimeout: 10000,
       retryStrategy: times => Math.min(times * 50, 2000)
     };
     pubsub = new RedisPubSub({
-      publisher: new Redis(options),
-      subscriber: new Redis(options)
+      publisher: new Redis(mockOptions),
+      subscriber: new Redis(mockOptions)
     });
+
     expect(pubsub.redisPublisher.options.host).toBe('localhost');
     expect(pubsub.redisPublisher.options.port).toBe(6378);
     expect(pubsub.redisSubscriber.options.host).toBe('localhost');
     expect(pubsub.redisSubscriber.options.port).toBe(6378);
+  });
+  it('should retry after the given time ', () => {
+    const spy = jest.spyOn(options, 'retryStrategy');
+    const res = spy(3);
+    expect(res).toEqual(150);
   });
 });
