5aea042532b7771af88b2b1fc2870a957d1cd4a4
Anurag-Wednesday <anurag@wednesday.is>
2022-03-07 12:04:15 +0530

    added tests for queue and fixed issue with annotations

---
+++ __mocks__/bull.js
@@ -0,0 +1,15 @@
+export default function(msg) {
+  const done = () => {
+    console.log('done');
+  };
+  const job = {
+    id: 1,
+    data: {
+      message: 'This is a sample job'
+    }
+  };
+  return {
+    data: msg,
+    process: fn => fn(job, done)
+  };
+}
