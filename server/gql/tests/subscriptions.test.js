1b92a92dc9b03c030434ee2c942547521d0535a7
Anurag-Wednesday <anurag@wednesday.is>
2022-03-09 15:49:12 +0530

    added tests for subsriptions

---
+++ server/gql/tests/subscriptions.test.js
@@ -0,0 +1,26 @@
+import { getResponse } from '@utils/testUtils';
+import * as module from 'graphql-subscriptions';
+import '@utils/pubsub';
+
+describe('Subscription tests', () => {
+  beforeAll(() => {
+    jest.unmock('graphql-redis-subscriptions');
+    jest.unmock('ioredis');
+  });
+
+  it('should add a subscription', async () => {
+    const subscription = `
+    subscription Subscription{
+        notifications(supplierId: 1671) {
+          price
+          supplierId
+          deliveryDate
+          productId
+        }
+      }
+  `;
+    const spy = jest.spyOn(module, 'withFilter');
+    await getResponse(subscription);
+    expect(spy).toBeCalled();
+  });
+});
