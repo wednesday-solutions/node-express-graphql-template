c1f0426098142f933bdd191eb8872d6410ed8459
Anurag-Wednesday <anurag@wednesday.is>
2022-03-10 15:40:39 +0530

    updated tests for pubsub

--- server/utils/tests/pubsub.test.js
+++ server/utils/tests/pubsub.test.js
@@ -1,20 +1,14 @@
-import { options } from '@utils/pubsub';
-
-import * as module from '@utils/pubsub';
-
 describe('Pubsub tests', () => {
-  let pubsub;
-  it('should ', () => {
-    const spy = jest.spyOn(options, 'retryStrategy');
+  it('should create a new Pubsub', () => {
+    const graphqlSubscription = require('graphql-redis-subscriptions');
+    const spy = jest.spyOn(graphqlSubscription, 'RedisPubSub');
+    require('@utils/pubsub');
+    expect(spy).toBeCalledTimes(1);
+  });
+  it('should set the retry stratergy ', () => {
+    const module = require('@utils/pubsub');
+    const spy = jest.spyOn(module.options, 'retryStrategy');
     const res = spy(3);
     expect(res).toEqual(150);
-  });
-  it('should ', () => {
-    pubsub = module.createPubSub();
-    expect(pubsub.redisPublisher.options.host).toBe('localhost');
-    expect(pubsub.redisPublisher.options.port).toBe(6379);
-    expect(pubsub.redisSubscriber.options.host).toBe('localhost');
-    expect(pubsub.redisSubscriber.options.port).toBe(6379);
-    pubsub.close();
   });
 });
