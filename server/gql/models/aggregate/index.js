6e38dd9af363933789713bc1a6abf647647df408
Anurag-Wednesday <anurag@wednesday.is>
2022-03-11 10:56:13 +0530

    update aggregate data on mutation

--- server/gql/models/aggregate/index.js
+++ server/gql/models/aggregate/index.js
@@ -1,7 +1,7 @@
 import { GraphQLFloat, GraphQLObjectType, GraphQLString } from 'graphql';
 import { GraphQLDateTime } from 'graphql-iso-date';
 import { client } from '@database';
-import { handleAggregateQueries, queryOptions, queryRedis } from './purchasedProductsUtils';
+import { handleAggregateQueries, queryOptions } from './purchasedProductsUtils';
 
 const Aggregate = new GraphQLObjectType({
   name: 'Aggregate',
@@ -13,7 +13,11 @@ const Aggregate = new GraphQLObjectType({
           purchasedProductsPrice: {
             name: 'TotalPriceOfPurchasedProducts',
             type: GraphQLFloat,
-            resolve: async args => queryRedis('total', args)
+            resolve: async args => {
+              const query = `SELECT SUM(price) from purchased_products`;
+              const { where, join } = handleAggregateQueries(args, 'purchased_products');
+              return (await client.query(`${query} ${join} ${where};`, queryOptions(args)))[0].sum || 0;
+            }
           }
         })
       }),
@@ -43,7 +47,11 @@ const Aggregate = new GraphQLObjectType({
           purchasedProducts: {
             name: 'CountOfPurchasedProducts',
             type: GraphQLFloat,
-            resolve: async args => queryRedis('count', args)
+            resolve: async args => {
+              const query = `SELECT COUNT(*) from purchased_products`;
+              const { where, join } = handleAggregateQueries(args, 'purchased_products');
+              return (await client.query(`${query} ${join} ${where};`, queryOptions(args)))[0].count || 0;
+            }
           }
         })
       }),
