7b3c7b7bcc0d6d5d5a0b00d157e6514d1740b7b2
Anurag-Wednesday <anurag@wednesday.is>
2022-03-14 20:23:57 +0530

    fixed lint errors

--- server/gql/models/purchasedProducts/customCreateResolver.js
+++ server/gql/models/purchasedProducts/customCreateResolver.js
@@ -2,12 +2,10 @@ import moment from 'moment';
 import { getCategoryById, insertPurchasedProducts } from '@daos/purchasedProducts';
 import { transformSQLError } from '@utils';
 import { redis } from '@services/redis';
-import { getCategoryById } from '@utils/queries';
 import { SUBSCRIPTION_TOPICS } from '@server/utils/constants';
 import { getSingleSupplierId } from '@server/utils/rawQueries';
 import { pubsub } from '@server/utils/pubsub';
 import db from '@server/database/models';
-
 
 export const updateRedis = async res => {
   const currentDate = moment().format('YYYY-MM-DD');
