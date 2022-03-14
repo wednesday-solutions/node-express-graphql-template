a03a7f9ac831b6cff59e3e72c496178e5f361cba
Anurag-Wednesday <anurag@wednesday.is>
2022-03-14 16:12:10 +0530

    added publish message on mutation

--- server/gql/models/purchasedProducts/customCreateResolver.js
+++ server/gql/models/purchasedProducts/customCreateResolver.js
@@ -3,6 +3,10 @@ import { insertPurchasedProducts } from '@daos/purchasedProducts';
 import { transformSQLError } from '@utils';
 import { redis } from '@services/redis';
 import { getCategoryById } from '@utils/queries';
+import { SUBSCRIPTION_TOPICS } from '@server/utils/constants';
+import { getSingleSupplierId } from '@server/utils/rawQueries';
+import { pubsub } from '@server/utils/pubsub';
+import db from '@server/database/models';
 
 export const updateRedis = async res => {
   const currentDate = moment().format('YYYY-MM-DD');
@@ -24,10 +28,23 @@ export const updateRedis = async res => {
     })
   );
 };
+
+export const publishMessage = async (args, res) => {
+  const supplierProduct = await getSingleSupplierId(db.supplierProducts, args);
+  pubsub.publish(SUBSCRIPTION_TOPICS.NOTIFICATIONS, {
+    notifications: {
+      productId: res.productId,
+      deliveryDate: res.deliveryDate,
+      price: res.price,
+      supplierId: supplierProduct.supplierId
+    }
+  });
+};
 export default async (model, args, context) => {
   try {
     const res = await insertPurchasedProducts(args);
     updateRedis(res);
+    publishMessage(res, args);
     return res;
   } catch (err) {
     throw transformSQLError(err);
