c48b8b84ed6643e2316d2985706d1f2d3ccce922
Anurag-Wednesday <anurag@wednesday.is>
2022-03-17 15:58:34 +0530

    added support for transactions

--- server/database/index.js
+++ server/database/index.js
@@ -1,17 +1,45 @@
 import Sequelize from 'sequelize';
 import * as pg from 'pg';
-let client;
+import { isTestEnv, logger } from '@server/utils';
 
-export const getClient = () => {
-  if (!client) {
+let client;
+let namespace;
+const cls = require('cls-hooked');
+export const getClient = force => {
+  if (!namespace) {
+    namespace = cls.createNamespace(`${process.env.ENVIRONMENT_NAME}-namespace`);
+  }
+  if (force || !client) {
     try {
+      if (!isTestEnv()) {
+        Sequelize.useCLS(namespace);
+      }
       client = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
         host: process.env.POSTGRES_HOST,
         dialectModule: pg,
-        dialect: 'postgres'
+        logging: isTestEnv() ? false : console.log,
+        dialect: 'postgres',
+        pool: {
+          min: 0,
+          max: 10,
+          idle: 10000
+        },
+        retry: {
+          match: [
+            'unknown timed out',
+            Sequelize.TimeoutError,
+            'timed',
+            'timeout',
+            'TimeoutError',
+            'Operation timeout',
+            'refuse',
+            'SQLITE_BUSY'
+          ],
+          max: 10 // maximum amount of tries
+        }
       });
     } catch (err) {
-      console.error({ err });
+      logger().info({ err });
       throw err;
     }
   }
