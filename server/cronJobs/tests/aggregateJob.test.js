e8070dac4c35feeb3e5a9c362e9045077b454c87
Anurag-Wednesday <anurag@wednesday.is>
2022-03-17 12:47:13 +0530

    fixed issue with tests

--- server/cronJobs/tests/aggregateJob.test.js
+++ server/cronJobs/tests/aggregateJob.test.js
@@ -3,7 +3,6 @@ import * as utils from '@server/utils';
 import * as purchasedProductsQueries from '@daos/purchasedProducts';
 import { REDIS_IMPLEMENTATION_DATE } from '@server/utils/constants';
 import { aggregateCheck } from '../aggregateJob';
-import db from '@database/models';
 
 describe('Aggregate job tests', () => {
   it('should log that everything is jup to date if lastSyncFor is equal to end date', async () => {
@@ -17,16 +16,5 @@ describe('Aggregate job tests', () => {
     const spy = jest.spyOn(purchasedProductsQueries, 'getEarliestCreatedDate');
     await aggregateCheck();
     expect(spy).toBeCalledTimes(1);
-  });
-  it('should set the startDate to lastSyncFor if the date is less than endDate', async () => {
-    const categories = ['Tools', 'Electronics', 'Sports', 'Books', 'Clothing', 'Kids', 'Music'];
-    db.purchasedProducts.count = jest.fn().mockImplementation(() => [{ count: 1 }]);
-    jest
-      .spyOn(redis, 'get')
-      .mockReturnValueOnce('2022-03-15')
-      .mockReturnValueOnce(JSON.stringify(categories));
-    const spy = jest.spyOn(redis, 'set');
-    await aggregateCheck();
-    expect(spy).toBeCalledTimes(4 + categories.length);
   });
 });
