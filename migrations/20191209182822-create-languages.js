b668bfe4bf5daf1260f396252fc91de87b5a9464
Baipalli Sagar <baipalli@wednesday.is>
2022-09-26 19:26:09 +0530

    fix: deleted the  migration file of create languages

--- migrations/20191209182822-create-languages.js
+++
@@ -1,6 +0,0 @@
-const { migrate } = require('../server/utils/migrateUtils');
-
-module.exports = {
-  up: queryInterface => migrate(__filename, queryInterface),
-  down: () => Promise.reject(new Error('error'))
-};
