7630d5e5d975f2db1fb2a790e8ba9605f32243a6
Baipalli Sagar <baipalli@wednesday.is>
2022-09-30 13:04:41 +0530

    chore: refactoring

--- server/daos/languages.js
+++ server/daos/languages.js
@@ -1,14 +1,16 @@
 import db from '@database/models';
+import { logger } from '@utils/index';
 
 export const insertLanguage = args => db.books.create(args);
 
 export const updateLanguage = async args => {
   const mapLanguageUpdateArgs = {
-    name: args.name,
-    country: args.country
+    language: args.language
   };
 
-  await db.languages.update(mapLanguageUpdateArgs, { where: { id: args.id } });
+  const languageResponse = await db.languages.update(mapLanguageUpdateArgs, { where: { id: args.id } });
+
+  logger().info(languageResponse);
 
   const language = await db.languages.findOne({ where: { id: args.id } });
 
